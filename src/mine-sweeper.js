c6105be8baff8732598f99913d163f4b33237788
archdemon7 <vakylskij@gmail.com>
2022-10-17 17:50:40 +0300

    feat: mine sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function minesweeper(matrix) {
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    result.push([])
+    for (let j = 0; j < matrix[0].length; j++) {
+      let sum = 0;
+      for (let t = -1; t <= 1; t++) {
+        for (let p = -1; p <= 1; p++) {
+          if(t === 0 && p === 0) {continue}
+          if (matrix[i + t] && matrix[i + t][j + p] && matrix[i + t][j + p] === true) {sum++}
+          }
+        }
+      result[i].push(sum)
+    }
+  }
+  return result
 }
 
 module.exports = {
