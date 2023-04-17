2473ee6a172727d428a3dbc3c764c13f1ebaf2fb
archdemon7 <vakylskij@gmail.com>
2023-04-17 21:05:47 +0300

    feat: add solution vegener

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(flag = true) {
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    this.result = [];
+    this.flag = flag;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  createSquare() {
+    for (let i = 0; i < this.alphabet.length; i++) {
+      this.result[i] = [];
+      for (let j = 0; j < this.alphabet.length; j++) {
+        this.result[i][j] = this.alphabet[j];
+      }
+      this.alphabet.push(this.alphabet.shift());
+    }
+  }
+
+  encrypt(message, key) {
+    this.createSquare();
+    if (!(message && key)) {
+      throw new Error('Incorrect arguments!');
+    }
+    let encryptMessage = '';
+    if (key.length < message.length) {
+      key = key.repeat(message.length);
+    }
+    let keyIndex = 0;
+    for (let i = 0; i < message.length; i++) {
+        if (message[i].toUpperCase().charCodeAt() > 64 && message[i].toUpperCase().charCodeAt() < 91) {
+          encryptMessage += this.result[this.alphabet.indexOf(key[keyIndex].toUpperCase())][this.alphabet.indexOf(message[i].toUpperCase())];
+          keyIndex++;
+        } else {
+          encryptMessage += message[i];
+        }
+      }
+    return this.flag ? encryptMessage : encryptMessage.split('').reverse().join('');
+  }
+  decrypt(message, key) {
+    this.createSquare();
+    if (!(message && key)) {
+      throw new Error('Incorrect arguments!');
+    }
+    let decryptMessage = '';
+    if (key.length < message.length) {
+      key = key.repeat(message.length);
+    }
+    let keyIndex = 0;
+    for (let i = 0; i < message.length; i++) {
+        if (message[i].toUpperCase().charCodeAt() > 64 && message[i].toUpperCase().charCodeAt() < 91){
+          decryptMessage += this.alphabet[this.result[this.alphabet.indexOf(key[keyIndex].toUpperCase())].indexOf(message[i])];
+          keyIndex++;
+        } else {
+          decryptMessage += message[i];
+        }
+      }
+    return this.flag ? decryptMessage : decryptMessage.split('').reverse().join('');
   }
 }
 
